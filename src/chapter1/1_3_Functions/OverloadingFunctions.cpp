5f3484dae57c0eb059c2f54d819d060ec377d585
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-28 14:03:12 +0100

    feat : OverloadingFunctions.cpp/h, print with int parameters function

---
+++ src/chapter1/1_3_Functions/OverloadingFunctions.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Baran on 12/28/2024.
+//
+#include <iostream>
+void print(int value){
+    std::cout << "the value is : " << value;
+}
+#include "OverloadingFunctions.h"
---
+++ src/chapter1/1_3_Functions/OverloadingFunctions.h
@@ -0,0 +1,11 @@
+//
+// Created by Baran on 12/28/2024.
+//
+#ifndef ATOCPP_OVERLOADINGFUNCTIONS_H
+#define ATOCPP_OVERLOADINGFUNCTIONS_H
+void print(int value);
+//void print(double value);
+//void print(const std::string& value);
+//void ambiguous(int, double);
+//void ambiguous(double, int);
+#endif //ATOCPP_OVERLOADINGFUNCTIONS_H
