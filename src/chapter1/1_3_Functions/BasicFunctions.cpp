4b524328982d2bcd865124d9634162e171c4492c
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-28 13:54:51 +0100

    feat : BasicFunctions.cpp/h, sqrt function

---
+++ src/chapter1/1_3_Functions/BasicFunctions.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Baran on 12/28/2024.
+//
+#include <cmath>
+#include "BasicFunctions.h"
+double sqrt(double value){
+    return std::pow(value,0.5); // could use std::sqrt function
+}
---
+++ src/chapter1/1_3_Functions/BasicFunctions.h
@@ -0,0 +1,11 @@
+//
+// Created by Baran on 12/28/2024.
+//
+
+#ifndef ATOCPP_BASICFUNCTIONS_H
+#define ATOCPP_BASICFUNCTIONS_H
+
+double sqrt(double value);
+
+
+#endif //ATOCPP_BASICFUNCTIONS_H
