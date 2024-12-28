7e93280be3921b9e7de7e78ea3958507fbe44ec7
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-28 13:21:42 +0100

    feat : HelloWorld.cpp/h, HelloWorld() function

---
+++ src/chapter1/1_2_Programs/HelloWorld.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Baran on 12/28/2024.
+//
+#include <iostream>
+#include "HelloWorld.h"
+void HelloWorld(){
+    std::cout << "Hello, World!\n";
+}
---
+++ src/chapter1/1_2_Programs/HelloWorld.h
@@ -0,0 +1,11 @@
+//
+// Created by Baran on 12/28/2024.
+//
+
+#ifndef ATOCPP_HELLOWORLD_H
+#define ATOCPP_HELLOWORLD_H
+
+void HelloWorld();
+
+#endif //ATOCPP_HELLOWORLD_H
+
