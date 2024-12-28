281e40a5e03171bd9665a768db320199e09bc51a
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-28 18:03:52 +0100

    feat : Vector functions
    - Constructor (not implemented)
    - Destructor (not implemented)
    - operator (not implemented)
    - size (implemented)

---
+++ src/chapter2/2_3_Classes/Vector.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Baran on 12/28/2024.
+//
+
+#include "Vector.h"
+
+Vector::Vector(int size) {
+
+}
+
+Vector::~Vector() {
+
+}
+
+// Member function: Access elements in the vector
+double& Vector::operator[](int index) {
+
+}
+
+// Member function: Get the size of the vector
+int Vector::size() const {
+    return this->sz;
+}
