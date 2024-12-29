5a190d2d27e5a1329c0e94e6b18be3ccc463f885
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-29 19:51:45 +0100

    fix : added My_Func into the file

--- src/chapter1/1_3_Functions/source/BasicFunctions.cpp
+++ src/chapter1/1_3_Functions/source/BasicFunctions.cpp
@@ -3,13 +3,16 @@
 //
 #include <cmath>
 #include "../include/BasicFunctions.h"
+namespace My_Func {
+    double sqrt(double value) {
+        return std::pow(value, 0.5); // could use std::sqrt function
+    }
 
-double sqrt(double value){
-    return std::pow(value,0.5); // could use std::sqrt function
-}
-double square(double value){
-    return value*value; // could use std::pow(value,2)
-}
-int* next_elem(int* ptr){
-    return ptr+1; // can be use to iterate through an int array
+    double square(double value) {
+        return value * value; // could use std::pow(value,2)
+    }
+
+    int *next_elem(int *ptr) {
+        return ptr + 1; // can be use to iterate through an int array
+    }
 }
