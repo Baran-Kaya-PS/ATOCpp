bfa8b93a4f3ae4b9ddce5b6320c28a7e47639b25
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-29 16:25:59 +0100

    feat : TrafficLight enumeration values

---
+++ src/chapter2/2_4_Enumeration/TrafficLight.h
@@ -0,0 +1,13 @@
+//
+// Created by Baran on 12/29/2024.
+//
+#include <iostream>
+#ifndef ATOCPP_TRAFFICLIGHT_H
+#define ATOCPP_TRAFFICLIGHT_H
+
+
+enum class TrafficLight {
+    RED,YELLOW,GREEN
+};
+
+#endif //ATOCPP_TRAFFICLIGHT_H
