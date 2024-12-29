dd2e636fc55a51a2362d33dc9e0d96dcf7ce83ac
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-29 19:51:16 +0100

    test : testing My_Func::sqrt(x), worked, working on test files...

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include "chapter1/1_2_Programs/include/HelloWorld.h"
 #include "../tests/chapter2_tests/test_TrafficLight.h"
+#include "chapter1/1_3_Functions/include/BasicFunctions.h"
+using namespace My_Func;
+
 int main() {
-    runTrafficLightTests();
+    int number;
+    std::cout << "Enter a number : " << std::endl;
+    std::cin >> number;
+    std::cout << "the square root of " << number << " is : " << sqrt(number) << std::endl;
     return 0;
 }
