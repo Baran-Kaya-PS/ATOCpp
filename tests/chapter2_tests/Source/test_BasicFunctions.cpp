0bb9673342030e4f27fa8c9d621361c6a5ef2b35
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-30 16:41:50 +0100

    test : new test, SqrtNegative return nan value

--- tests/chapter2_tests/Source/test_BasicFunctions.cpp
+++ tests/chapter2_tests/Source/test_BasicFunctions.cpp
@@ -14,3 +14,7 @@ TEST(BasicFunctionTest,SqrtPositiveValue){
 TEST(BasicFunctionTest,SqrtZero){
     EXPECT_NEAR(My_Func::sqrt(0.0),0,1e-9);
 }
+
+TEST(BasicFunctionTest, SqrtNegativeValue){
+    EXPECT_TRUE(std::isnan(My_Func::sqrt(-1.0)));
+}
