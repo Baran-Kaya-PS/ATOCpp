edd20ae75f50978293520c421bb64f7504f6239d
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-30 16:33:42 +0100

    test : SqrtPositiveValue

--- tests/chapter2_tests/Source/test_BasicFunctions.cpp
+++ tests/chapter2_tests/Source/test_BasicFunctions.cpp
@@ -1,5 +1,12 @@
 //
 // Created by Baran on 12/29/2024.
 //
-
+#include "../../../src/chapter1/1_3_Functions/Include/BasicFunctions.h"
 #include "../Include/test_BasicFunctions.h"
+#include <gtest/gtest.h>
+#include <cmath>
+
+
+TEST(BasicFunctionTest,SqrtPositiveValue){
+    EXPECT_NEAR(My_Func::sqrt(16),4,1e-9);
+}
