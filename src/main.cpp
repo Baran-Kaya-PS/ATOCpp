862460ea3a00e965831f08782a99df8f74f89553
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-30 17:03:37 +0100

    test : - all the tests are running 6 tests passed, 1 test did not
    failed test is BasicFunctionTest.NextElemNullPointer

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
-#include <iostream>
-#include "chapter1/1_2_Programs/include/HelloWorld.h"
-#include "../tests/chapter2_tests/test_TrafficLight.h"
-#include "chapter1/1_3_Functions/include/BasicFunctions.h"
-using namespace My_Func;
-
-int main() {
-    int number;
-    std::cout << "Enter a number : " << std::endl;
-    std::cin >> number;
-    std::cout << "the square root of " << number << " is : " << sqrt(number) << std::endl;
-    return 0;
+#include <gtest/gtest.h>
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv); // Initialisation avec argc/argv
+    int testResult = RUN_ALL_TESTS(); // Exécution des tests
+    std::cout << "Tests completed with result: " << testResult << std::endl;
+    return testResult;
 }
