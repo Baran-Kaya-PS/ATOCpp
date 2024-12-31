4fb80de4a952dc7574a30c4c10cb745620d52af6
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2024-12-31 14:01:41 +0100

    test : Vector class testing, NormalUsage

--- tests/chapter2_tests/Source/test_Vector.cpp
+++ tests/chapter2_tests/Source/test_Vector.cpp
@@ -2,7 +2,6 @@
 // Created by Baran on 12/31/2024.
 //
 #include "../../../src/chapter2/2_3_Classes/source/Vector.cpp"
-#include "test_Vector.h"
 #include "gtest/gtest.h"
 TEST(VectorTest,ConstructorNegativeSize){
     EXPECT_THROW(Vector(-1),std::length_error);
@@ -12,4 +11,19 @@ TEST(VectorTest,OutofBoundsAccess){
     Vector v(5);
     EXPECT_THROW(v[10] = 42, std::out_of_range);
     EXPECT_THROW(v[-1] = 2, std::out_of_range);
+}
+TEST(VectorTest, NormalUsage) {
+    Vector v(5);
+    v[0] = 1.1;
+    v[1] = 2.2;
+    v[2] = 3.3;
+    v[3] = 4.4;
+    v[4] = 5.5;
+
+    EXPECT_EQ(v.size(), 5);
+    EXPECT_DOUBLE_EQ(v[0], 1.1);
+    EXPECT_DOUBLE_EQ(v[1], 2.2);
+    EXPECT_DOUBLE_EQ(v[2], 3.3);
+    EXPECT_DOUBLE_EQ(v[3], 4.4);
+    EXPECT_DOUBLE_EQ(v[4], 5.5);
 }
