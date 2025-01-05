50dd21aca3107a651d0f0d04a2d695f4de5f7f35
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-01-05 19:07:44 +0100

    feat : List_Container class, constructor with std::initializer_list

--- src/chapter5/Include/List_Container.h
+++ src/chapter5/Include/List_Container.h
@@ -5,14 +5,14 @@
 #ifndef ATOCPP_LIST_CONTAINER_H
 #define ATOCPP_LIST_CONTAINER_H
 #include <list>
-
+#include <initializer_list>
 
 class List_Container {
 private:
     std::list<double> Id;
 public:
-    List_Container(){};
-
+    List_Container(){}
+    List_Container(std::initializer_list<double> il) : Id(il) {};
 };
 
 
