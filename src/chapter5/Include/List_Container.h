f613b8e0e181b9733a073c200b549e8c115f5f64
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-01-05 19:04:07 +0100

    feat : List_Container class, the purpose is that we can implement differently with the same commun interface,
    purpose for after: make a function with Container as input and use it on Vector_container and List_Container.

---
+++ src/chapter5/Include/List_Container.h
@@ -0,0 +1,15 @@
+//
+// Created by Baran on 1/5/2025.
+//
+
+#ifndef ATOCPP_LIST_CONTAINER_H
+#define ATOCPP_LIST_CONTAINER_H
+#include <list>
+
+
+class List_Container {
+
+};
+
+
+#endif //ATOCPP_LIST_CONTAINER_H
---
+++ src/chapter5/Source/List_Container.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Baran on 1/5/2025.
+//
+
+#include "../Include/List_Container.h"
